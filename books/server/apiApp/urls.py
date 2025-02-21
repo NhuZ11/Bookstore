4623b5b2e4d59901a61dab4e81723d5b492eba9c
NhuZ11 <nhuzbajra59@gmail.com>
2025-02-21 09:39:02 +0545

    (nhuz:backend authenticaiton api)

--- books/server/apiApp/urls.py
+++ books/server/apiApp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-from .views import BookListCreate, BookDetailUpdateDelete
+from .views import BookListCreate, BookDetailUpdateDelete , RegisterView, LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path("books/", BookListCreate.as_view(), name="book-list"),
     path("books/<int:pk>/delete/", BookDetailUpdateDelete.as_view(), name="book-detail-delete"),
     path("books/<int:pk>/update/", BookDetailUpdateDelete.as_view(), name="book-detail-update"),
     path("books/<int:pk>/", BookDetailUpdateDelete.as_view(), name="book-detail-view-specific"),
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- books/server/server/settings.py
+++ books/server/server/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apiApp',
+    'rest_framework_simplejwt',
 ]
 
 
@@ -132,3 +133,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#manual add
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+       'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
